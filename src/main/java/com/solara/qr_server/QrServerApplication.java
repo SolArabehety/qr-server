0ab7bb5284d5c51165af1222926e691fdfa68a43
SolArabehety <sol.arabehety@gmail.com>
2025-05-28 14:30:31 -0300

    initial commit

---
+++ src/main/java/com/solara/qr_server/QrServerApplication.java
@@ -0,0 +1,13 @@
+package com.solara.qr_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrServerApplication.class, args);
+	}
+
+}
